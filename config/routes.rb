798773ad42adaa8ea1e9433fb01af4bcf0d053bb
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2016-08-02 00:17:30 -0700

    clean code

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Naturesoft::Core::Engine.routes.draw do
   devise_for :users, class_name: "Naturesoft::User", module: :devise
   root to: "home#index"
-  namespace :admin, path: "admin" do
+  namespace :admin do
     get '/' => 'dashboard#index', as: :dashboard
     resources :users, only: :index
   end

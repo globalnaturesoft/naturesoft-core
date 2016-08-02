3e03783b09aea64023d0106b73a5e804913591c1
Luan Pham <luanpm88@gmail.com>
2016-08-01 23:29:42 -0700

    update route admin scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Naturesoft::Core::Engine.routes.draw do
   devise_for :users, class_name: "Naturesoft::User", module: :devise
   root to: "home#index"
-  namespace :admin, path: "naturesoft-cms" do
+  namespace :admin, path: "admin" do
       get '/' => 'dashboard#index', as: :dashboard
       resources :users, only: :index
   end

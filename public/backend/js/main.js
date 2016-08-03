3a9f08f9e1060827a3ed6864fe3defa75e1f10b7
Luan Pham <luanpm88@gmail.com>
2016-08-02 23:35:59 -0700

    update layout

--- public/backend/js/main.js
+++ public/backend/js/main.js
@@ -1,4 +1,7 @@
-$(function() {    
+$(document).ready(function() {    
     // Default initialization
     $(".styled").uniform();
+    
+    // Add active class to last breadscrumb
+    $(".breadcrumb li").last().addClass("active");
 });

a70f34ce016e5aa32531a86aae0136bf8a863ff8
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2016-08-03 21:15:24 -0700

    add style radio

--- public/backend/js/main.js
+++ public/backend/js/main.js
@@ -4,4 +4,11 @@ $(document).ready(function() {
     
     // Add active class to last breadscrumb
     $(".breadcrumb li").last().addClass("active");
+    
+    // Checkboxes and radios
+    $(".styled-level-1, .multiselect-container input").uniform({
+        radioClass: 'choice',
+        checkboxClass: 'checker',
+        wrapperClass: "border-teal text-teal-600"
+    });
 });

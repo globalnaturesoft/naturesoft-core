24830876ea3b47009fbe9c3dd59f12f8f8067173
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-08-07 19:14:13 -0700

    display status for general use

--- app/helpers/naturesoft/application_helper.rb
+++ app/helpers/naturesoft/application_helper.rb
@@ -7,5 +7,35 @@ module Naturesoft
     def title(page_title)
       content_for :title, page_title.to_s
     end
+    
+    # display status
+    def display_status(status)
+      statuses = {
+        'active': {
+          'label': 'Active',
+          'class': 'success'
+        },
+        'inactive': {
+          'label': 'Inactive',
+          'class': 'default'
+        },
+      }
+      return "<span class=\"label label-#{statuses[:"#{status}"][:'class']}\">#{statuses[:"#{status}"][:'label']}</span>".html_safe
+    end
+    
+    # display button enable/disable for status
+    def display_btn_status(status)
+      statuses = {
+        'active': {
+          'label': 'Disable',
+          'class': 'lock2'
+        },
+        'inactive': {
+          'label': 'Enable',
+          'class': 'unlocked2'
+        },
+      }
+      return "<i class=\"icon-#{statuses[:"#{status}"][:'class']}\"></i> #{statuses[:"#{status}"][:'label']}".html_safe
+    end
   end
 end

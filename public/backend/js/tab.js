4284776bbb8a687734cff54fa93bcb41ae2e726b
Luan Pham <luanpm88@gmail.com>
2016-08-08 01:59:40 -0700

    update tab name

--- public/backend/js/tab.js
+++ public/backend/js/tab.js
@@ -1,3 +1,8 @@
+function setTabName(url, name) {
+	alert(name);
+	$('a[data-src="'+url+'"] .tab_name').html(name);
+}
+
 function removeTabUrl(url) {
 	var index = tab_urls.indexOf(url);
 	while (index != -1) {
@@ -29,7 +34,7 @@ function openTab(url, name, hide_close) {
 	}
 	
 	var id = randomString(10);
-	$(".ns-main-tabs ul.nav").append('<li><a data-src="'+url+'" href="#'+id+'" data-toggle="tab">'+name+' <span class="tabs-buts"><i class="icon-reload-alt tab-refresh"></i> '+close_but+'</span></a></li>');
+	$(".ns-main-tabs ul.nav").append('<li><a data-src="'+url+'" href="#'+id+'" data-toggle="tab"><span class="tab_name">'+name+'</span> <span class="tabs-buts"><i class="icon-reload-alt tab-refresh"></i> '+close_but+'</span></a></li>');
 	$(".ns-main-tabs .tab-content").append('<div class="tab-pane" id="'+id+'"><iframe scrolling="no" src="'+url+'"></iframe></div>');
 	
 	// log tab actione
@@ -196,6 +201,13 @@ $(document).ready(function() {
 		refreshTab(url);
 	});
 	
-	
+	// update tab title
+	if (window.frameElement) {
+		var title = document.title.split(" - ")[0];
+		var url = window.location.href.replace(/^.*\/\/[^\/]+/, '');
+		if(url != "/admin/dashboard" && title != "") {
+			parent.setTabName(url, title);
+		}
+	}
 	
 });

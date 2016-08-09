f9723b872832458a692da2b1202b3e7bce2fc7b1
Luan Pham <luanpm88@gmail.com>
2016-08-08 19:09:51 -0700

    remove core root_to route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Naturesoft::Core::Engine.routes.draw do
   devise_for :users, class_name: "Naturesoft::User", module: :devise
-  root to: "home#index"
   namespace :admin do
     get '/' => 'admin#index'
     get '/dashboard' => 'dashboard#index', as: :dashboard

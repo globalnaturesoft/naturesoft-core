49cccdc7cbe965e4fbe0fd9bcf11684713352a51
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2016-08-11 00:18:59 -0700

    add format_number to application_helper

--- app/helpers/naturesoft/application_helper.rb
+++ app/helpers/naturesoft/application_helper.rb
@@ -8,6 +8,21 @@ module Naturesoft
       content_for :title, page_title.to_s
     end
     
+    def format_number(number, vn = false, round = false, precision = nil)
+      prec = (number.to_f.round == number.to_f) ? 0 : 2
+      prec = 0 if round
+      
+      if !precision.nil?
+        prec = precision
+      end
+    
+      if vn
+        number_to_currency(number, precision: prec, separator: ",", unit: '', delimiter: ".")
+      else
+        number_to_currency(number, precision: prec, separator: ".", unit: '', delimiter: ",")
+      end
+    end
+    
     # display status
     def display_status(status)
       statuses = {

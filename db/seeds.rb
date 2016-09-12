0e71cabce02adcf2352e2dbfd1130148e62c2d07
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-09-11 20:07:08 -0700

    update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,2 @@
-Naturesoft::User.create :first_name => "Global", :last_name => "Naturesoft", :email => "admin@globalnaturesoft.com", :password => "aA456321@"
+group = Naturesoft::UserGroup.create(name: "Users")
+group.users.create :first_name => "Global", :last_name => "Naturesoft", :email => "admin@globalnaturesoft.com", :password => "aA456321@"

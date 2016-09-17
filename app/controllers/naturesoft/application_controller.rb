0c40fa03df554adc907e6dabe978e08a8bb36be2
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2016-09-16 20:36:34 -0700

    update core - include cart

--- app/controllers/naturesoft/application_controller.rb
+++ app/controllers/naturesoft/application_controller.rb
@@ -1,4 +1,14 @@
 module Naturesoft
   class ApplicationController < ActionController::Base
+    before_action :get_cart
+    
+    private
+      def get_cart
+        @cart = Naturesoft::Products::Cart.find(session[:cart_id])
+      rescue ActiveRecord::RecordNotFound
+        @cart = Naturesoft::Products::Cart.create
+        session[:cart_id] = @cart.id
+      end
+      
   end
 end

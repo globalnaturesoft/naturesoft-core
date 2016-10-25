e98a99dd40ce59e228c928c540fee6db56c3c74d
Luan Pham <luanpm88@gmail.com>
2016-10-25 00:55:00 -0700

    after login redirect frontend/backend

--- app/controllers/naturesoft/application_controller.rb
+++ app/controllers/naturesoft/application_controller.rb
@@ -1,4 +1,16 @@
 module Naturesoft
   class ApplicationController < ActionController::Base
+    private
+      def after_sign_in_path_for(resource_or_scope)
+        if session[:current_view] == 'frontend'
+          "/"
+        elsif session[:current_view] == 'backend'
+          naturesoft.admin_path
+        end
+      end
+      #
+      #def after_sign_up_path_for(resource_or_scope)
+      #  naturesoft.new_user_session_path
+      #end
   end
 end

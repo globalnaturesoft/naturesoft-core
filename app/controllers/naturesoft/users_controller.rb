5f935b8bba5fc73458deadd145948fc5dac33ee1
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-10-26 19:42:03 -0700

    update infomation user profile

--- app/controllers/naturesoft/users_controller.rb
+++ app/controllers/naturesoft/users_controller.rb
@@ -1,15 +1,47 @@
 module Naturesoft
-    class UsersController < FrontendController
-        def login
-        end
-        
-        def register
-        end
-        
-        def forgot_password
-        end
-        
-        def admin_area
-        end
+  class UsersController < FrontendController
+    def login
     end
+    
+    def register
+    end
+    
+    def forgot_password
+    end
+    
+    # PATCH/PUT /users/1
+    def update
+      params[:user].delete(:password) if params[:user][:password].blank?
+      params[:user].delete(:password_confirmation) if params[:user][:password_confirmation].blank?
+      
+      if @user.update(user_params)
+        redirect_to admin_area_path, notice: 'User was successfully updated.'
+      else
+        render :edit
+      end
+    end
+    
+    def admin_area
+      @user = current_user
+      
+      if params[:user].present?
+        params[:user].delete(:password) if params[:user][:password].blank?
+        params[:user].delete(:password_confirmation) if params[:user][:password_confirmation].blank?
+        
+        if @user.update(user_params)
+          redirect_to admin_area_path, notice: 'Account was successfully updated.'
+        end
+      end
+    end
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_user
+        @user = Naturesoft::User.find(params[:id])
+      end
+  
+      # Only allow a trusted parameter "white list" through.
+      def user_params
+        params.fetch(:user, {}).permit(:first_name, :last_name, :email, :phone, :address, :birthdate, :zip, :image, :password, :password_confirmation, :user_group_id)
+      end
+  end
 end

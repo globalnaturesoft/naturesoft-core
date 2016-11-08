5297560d7f6b85a6442c26abd5fe143ffe839d80
Luan Pham <luanpm88@gmail.com>
2016-11-07 23:39:56 -0800

    update user admin true in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 group = Naturesoft::UserGroup.create(name: "Users")
-group.users.create :first_name => "Global", :last_name => "Naturesoft", :email => "admin@globalnaturesoft.com", :password => "aA456321@"
+group.users.create :first_name => "Global", :last_name => "Naturesoft", :email => "admin@globalnaturesoft.com", :password => "aA456321@", admin: true

381ade62067b756b8915d8e06d6165151befe83a
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2016-11-08 00:38:34 -0800

    update

--- app/controllers/naturesoft/application_controller.rb
+++ app/controllers/naturesoft/application_controller.rb
@@ -14,7 +14,7 @@ module Naturesoft
     private      
       def after_sign_in_path_for(resource_or_scope)
         if session[:current_view] == 'frontend'
-          naturesoft.backend_area_path
+          naturesoft.admin_area_path
         elsif session[:current_view] == 'backend'
           naturesoft.backend_path
         end

86388c20933b6a607365d3e425694661b38954b2
Luan Pham <luanpm88@gmail.com>
2016-11-15 20:34:56 -0800

    redirect page when inactive signup

--- app/controllers/naturesoft/users/registrations_controller.rb
+++ app/controllers/naturesoft/users/registrations_controller.rb
@@ -1,21 +1,30 @@
 module Naturesoft
-    class Users::RegistrationsController  < Devise::RegistrationsController
-        layout :set_layout
-        
-        private
-        def set_layout
-            if session[:current_view] == 'frontend'
-                "naturesoft/frontend"
-            else
-                "naturesoft/login"
-            end
-        end
-        def sign_up_params
-            params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :user_group_id)
-        end
-        
-        def account_update_params
-            params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :user_group_id)
-        end
+  class Users::RegistrationsController  < Devise::RegistrationsController
+    layout :set_layout
+    
+    private
+    def set_layout
+      if session[:current_view] == 'frontend'
+        "naturesoft/frontend"
+      else
+        "naturesoft/login"
+      end
     end
+    
+    def sign_up_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :user_group_id)
+    end
+    
+    def account_update_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :user_group_id)
+    end
+    
+    def after_inactive_sign_up_path_for(resource_or_scope)
+      if session[:current_view] == 'frontend'
+        naturesoft.confirmable_email_path
+      elsif session[:current_view] == 'backend'
+        naturesoft.backend_path
+      end
+    end
+  end
 end
